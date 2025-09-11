3d9bcea7c9bbc56346ef60af4473ed187081462e
Lokesh Kumar <lokesh2.yss@gmail.com>
2025-09-12 00:12:11 +0530

    added memento design pattern

---
+++ src/main/java/behavioral/memento/TextEditorMemento.java
@@ -0,0 +1,12 @@
+package behavioral.memento;
+
+public class TextEditorMemento {
+    private final String state;
+
+    public TextEditorMemento(String state) {
+        this.state = state;
+    }
+    public String getState() {
+        return state;
+    }
+}
